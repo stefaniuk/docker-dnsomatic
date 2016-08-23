090fa7e4da0958d41987a61ce6eba3f7e4e6ecf3
Daniel Stefaniuk <daniel.stefaniuk@gmail.com>
2016-08-23 21:37:25 +0100

    Customise

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ ENV URL ${URL:-"https://updates.dnsomatic.com/nic/update"}
 ENV USERNAME ${USERNAME:-"test"}
 ENV PASSWORD ${PASSWORD:-"test"}
 ENV LAPSE ${LAPSE:-"300"}
+ENV DELAY ${DELAY:-"120"}
 ENV TIMEZONE ${TIMEZONE:-"Europe/London"}
 
 COPY dnsomatic.py /
--- dnsomatic.py
+++ dnsomatic.py
@@ -1,16 +1,25 @@
 import requests, time, os
 import logging, logging.config
 
+# get environment variables
 url = os.getenv('URL')
 username = os.getenv('USERNAME')
 password = os.getenv('PASSWORD')
-lapse = float(os.getenv('LAPSE'))
+lapse = int(os.getenv('LAPSE'))
+delay = int(os.getenv('DELAY'))
 timezone = os.getenv('TIMEZONE')
 
+# set timezone
 os.environ['TZ'] = timezone
 time.tzset()
+# set logger
 logging.config.fileConfig('logging.conf')
 log = logging.getLogger('dnsomatic')
+
+# delayed startup
+if delay > 1:
+    log.info('Script started with a ' + str(delay) + ' seconds delay')
+    time.sleep(delay)
 
 ip = ''
 while True:
@@ -20,7 +29,7 @@ while True:
             newIp = req.text.rsplit()[1]
             if newIp != ip:
                 ip = newIp
-                log.info('New IP ' + ip)
+                log.info('New IP set to ' + ip)
         else:
             log.error(req.text)
     except Exception as e:
